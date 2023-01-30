d07ffa0b1a33dc930b4b8fb5d0c0528a2f5b6d0b
gabopereyra <gabrielp1689@gmail.com>
2023-01-30 20:12:03 -0300

    Add components, modules and configured routing for lazy load. All not working yet, are the base for the exercises

--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
+import { BasicosComponent } from './basicos/basicos.component'; 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BasicosComponent
+  ],
   imports: [
     CommonModule,
     ReactiveRoutingModule
--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TemplateRoutingModule } from './template-routing.module';
+import { SwitchesComponent } from './switches/switches.component';
+import { BasicosComponent } from './basicos/basicos.component';
+import { DinamicosComponent } from './dinamicos/dinamicos.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SwitchesComponent,
+    BasicosComponent,
+    DinamicosComponent
+  ],
   imports: [
     CommonModule,
     TemplateRoutingModule
