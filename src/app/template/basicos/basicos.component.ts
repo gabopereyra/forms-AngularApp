6225f7191fe9f425d5cbb92174466181d2b5f96e
gabopereyra <gabrielp1689@gmail.com>
2023-02-01 20:27:06 -0300

    Add validation for price input

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -22,4 +22,11 @@ export class BasicosComponent implements OnInit {
     return this.miFormulario?.controls['producto']?.invalid && this.miFormulario?.controls['producto']?.touched;
   }
 
+  precioValido():boolean {
+    let touched : boolean = this.miFormulario?.controls['precio']?.touched;
+    let price : boolean = this.miFormulario?.controls['precio']?.value < 0;
+    let empty : boolean = this.miFormulario?.controls['precio']?.value == '' || this.miFormulario?.controls['precio']?.value == null
+    return (touched && price) || (touched && empty);
+  }
+
 }
