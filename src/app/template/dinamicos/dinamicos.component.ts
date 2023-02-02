73b57930018ee0032d415c2089cc4a1ef237bbc9
gabopereyra <gabrielp1689@gmail.com>
2023-02-02 20:49:47 -0300

    Add library for effects, use it to show and hide items from the list. Add method to add and delete items from the list

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,11 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Persona{
+  nombre : string;
+  favoritos : Favorito[]
+}
+
+interface Favorito{
+  id : number;
+  nombre : string;
+  activo : boolean;
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styleUrls: ['./dinamicos.component.css']
 })
 export class DinamicosComponent implements OnInit {
+  
+  public persona : Persona = {
+    nombre: "Gabo",
+    favoritos: [
+      {
+        id: 1,
+        nombre: "Metal Gear",
+        activo: true
+      },
+      {
+        id: 2,
+        nombre: "PES",
+        activo: true
+      },
+    ]
+  };
+
+  public listadoFavoritos : Favorito [] = [];
+
+  public nuevoJuego : string = "";
 
   constructor() { }
 
@@ -16,5 +47,27 @@ export class DinamicosComponent implements OnInit {
     console.log('se ha hecho click en el guardar');
   }
 
+  eliminar(id : number){
+    this.persona.favoritos[id].activo = false;
+    
+    this.borrarListado(id)
+  }
+
+  borrarListado(id : number){
+    setTimeout(()=>{
+      this.persona.favoritos.splice(id, 1);
+    }, 1000);
+  }
+
+  agregarJuego(){
+    const nuevoFavorito : Favorito ={
+      id: this.persona.favoritos.length+1,
+      nombre: this.nuevoJuego,
+      activo: true
+    };
+
+    this.persona.favoritos.push({...nuevoFavorito});
+    this.nuevoJuego = "";
+  }
 
 }
