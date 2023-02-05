ed89b2098cc917d961743be4ff2f83b78e8bca34
gabopereyra <gabrielp1689@gmail.com>
2023-02-05 17:26:39 -0300

    Add FormBuilder in TS

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -7,11 +7,19 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./basicos.component.css']
 })
 export class BasicosComponent implements OnInit {
-  miFormulario : FormGroup = new FormGroup({
-    'nombre': new FormControl('Desde TS')
+  // miFormulario : FormGroup = new FormGroup({
+  //   'nombre': new FormControl('Desde TS'),
+  //   'precio': new FormControl(0),
+  //   'existencias': new FormControl(0)
+  // })
+
+  miFormulario : FormGroup = this.fb.group({
+    'nombre': ['Desde TS'],
+    'precio': [0],
+    'existencias': [0]
   })
 
-  constructor() { }
+  constructor(private fb : FormBuilder) { }
 
   ngOnInit(): void {
   }
