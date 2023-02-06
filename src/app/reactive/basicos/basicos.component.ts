aac939ab0b0c9d5d4bc452c6407b8694bfb49ad3
gabopereyra <gabrielp1689@gmail.com>
2023-02-06 19:22:13 -0300

    Add (ngSubmit) in HTML, add method to be execute when the form is submitted

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -22,9 +22,24 @@ export class BasicosComponent implements OnInit {
   constructor(private fb : FormBuilder) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      'nombre': '',
+      'precio': 0
+    })
   }
 
   campoNoValido( campo : string ){
     return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched;
+  }
+
+  guardar(){
+    if(this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.miFormulario.value);
+    
+    this.miFormulario.reset();
   }
 }
