cfddc7d02b5cb34ce5fafb93271a34f8c4629d3f
gabopereyra <gabrielp1689@gmail.com>
2023-02-08 20:00:21 -0300

    Add form to switches for reactive examples

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  miFormulario : FormGroup = this.fb.group({
+    genero: ['', Validators.required],
+    notificaciones: [],
+    condiciones: [, Validators.requiredTrue]
+  })
+
+  persona = {
+    genero: "F",
+    notificaciones: true
+  }
+
+  constructor(private fb : FormBuilder) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset(this.persona);
   }
 
 }
