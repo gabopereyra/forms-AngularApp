bb49ceff6a75b9767ee6adca604f8de74b087be2
gabopereyra <gabrielp1689@gmail.com>
2023-02-10 16:20:50 -0300

    Add validation with regex for email field

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -7,10 +7,12 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
-  patternNombre = '([a-zA-A]+) ([a-zA-A]+)';
+  patternNombre : string= '([a-zA-A]+) ([a-zA-A]+)';
+  emailPattern : string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
   miForm : FormGroup = this.fb.group({
-    nombre: ['', [Validators.required, Validators.pattern(this.patternNombre)]]
+    nombre: ['', [Validators.required, Validators.pattern(this.patternNombre)]],
+    email: ['', [Validators.required, Validators.pattern(this.emailPattern)]]
   }) 
 
 
