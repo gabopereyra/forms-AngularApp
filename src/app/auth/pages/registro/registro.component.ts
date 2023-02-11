9223047a06a287bd6bf075f65d9cf072f7cc6c3b
gabopereyra <gabrielp1689@gmail.com>
2023-02-11 15:06:07 -0300

    Add personalized validation for username field

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -9,10 +9,23 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegistroComponent implements OnInit {
   patternNombre : string= '([a-zA-A]+) ([a-zA-A]+)';
   emailPattern : string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+  
+  adminNO(control : FormControl){
+    let valor : string = control.value?.trim().toLowerCase();
+    
+    if(valor == "admin"){
+      return {
+        noAdmin: true
+      }
+    }
+
+    return null;
+  }
 
   miForm : FormGroup = this.fb.group({
     nombre: ['', [Validators.required, Validators.pattern(this.patternNombre)]],
-    email: ['', [Validators.required, Validators.pattern(this.emailPattern)]]
+    email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
+    username: ['', [Validators.required, this.adminNO]]
   }) 
 
 
