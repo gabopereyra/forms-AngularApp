d9347e5f3ddd0fd13ee90237e99570edf04479dd
gabopereyra <gabrielp1689@gmail.com>
2023-02-12 13:32:30 -0300

    Add personalized message for errors in email input

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { EmailValidator, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmailValidator, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { EmailValidatorService } from 'src/app/shared/validators/email-validator.service';
 import { ValidatorService } from 'src/app/shared/validators/validator.service';
 
@@ -9,6 +9,19 @@ import { ValidatorService } from 'src/app/shared/validators/validator.service';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
+
+ get errorEmail() : string {
+    const errores = this.miForm.get('email')?.errors;
+    const touched = this.miForm.get('email')?.touched; 
+
+    if (errores?.['required'] && touched){
+      return "El email es obligatorio.";
+    } else if(errores?.['pattern'] && touched){
+      return "El formato es invalido.";
+    } else{
+      return "El email ya esta en uso.";
+    }
+  }
 
   miForm : FormGroup = this.fb.group({
     nombre: ['', [Validators.required, Validators.pattern(this.vs.patternNombre)]],
@@ -40,6 +53,31 @@ export class RegistroComponent implements OnInit {
   campoNoValido(campo : string){
     return this.miForm.get(campo)?.invalid && this.miForm.get(campo)?.touched; 
   }
+
+  //Example of another way to resolve personalized msg for email input
+  // emailRequired(){
+  //   let errores : any = this.miForm.get('email')?.errors;
+
+  //   let noExiste = (errores?.required) ? true : false;
+
+  //   return noExiste && this.miForm.get('email')?.touched
+  // }
+
+  // emailPattern(){
+  //   let errores : any = this.miForm.get('email')?.errors;
+
+  //   let patternNoValid = (errores?.pattern) ? true : false;
+
+  //   return patternNoValid && this.miForm.get('email')?.touched
+  // }
+
+  // emailExiste(){
+  //   let errores : any = this.miForm.get('email')?.errors;
+
+  //   let emailExiste = (errores?.emailExiste) ? true : false;
+
+  //   return emailExiste && this.miForm.get('email')?.touched
+  // }
 
   submitFormulario(){
     this.miForm.markAllAsTouched();
